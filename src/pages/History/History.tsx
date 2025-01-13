c11aeeb51d7f67737bb9ca3ba5ac499eda4b0d65
Thiago Leite <thiagofmleite@gmail.com>
2025-01-13 19:37:21 -0300

    feat: implement HistoryPage with styled components for task history display

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,3 +1,48 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
 export default function HistoryPage() {
-  return <h1>History</h1>;
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Nome da tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
