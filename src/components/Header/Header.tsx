1c1bce140155b7c8dad3840c857a3ad79cf15fc6
Thiago Leite <thiagofmleite@gmail.com>
2025-01-13 18:45:23 -0300

    feat: add logo and enhance Header component with navigation and styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,22 @@
+import { HeaderContainer } from "./styles";
+import logoIgnite from "../../assets/logo-ignite.svg";
+import { Scroll, Timer } from "@phosphor-icons/react";
+
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <HeaderContainer>
+      <img
+        src={logoIgnite}
+        alt=""
+      />
+      <nav>
+        <a href="">
+          <Timer size={24} />
+        </a>
+        <a href="">
+          <Scroll size={24} />
+        </a>
+      </nav>
+    </HeaderContainer>
   );
 }
