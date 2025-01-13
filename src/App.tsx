b4d53584f4fca09a709a4380faf8dcecef507814
Thiago Leite <thiagofmleite@gmail.com>
2025-01-13 18:16:13 -0300

    feat: integrate BrowserRouter and Router component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { Button } from "./components/Button";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
