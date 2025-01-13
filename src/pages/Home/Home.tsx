157e391dde1a90f376eb6631ea382cb941f0e152
Thiago Leite <thiagofmleite@gmail.com>
2025-01-13 19:05:23 -0300

    feat: implement HomePage with form and countdown timer styling

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,44 @@
+import { Play } from "@phosphor-icons/react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from "./styles";
+
 export default function HomePage() {
-  return <h1>Home</h1>;
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input
+            type="text"
+            id="task"
+            name="task"
+          />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input
+            type="number"
+            id="minutesAmount"
+            name="minutesAmount"
+          />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  );
 }
