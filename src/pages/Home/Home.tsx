7a94cb8d0983e35b78f3349a5b43b2dc81cfada5
Thiago Leite <thiagofmleite@gmail.com>
2025-01-13 19:26:55 -0300

    feat: add task suggestions and input constraints in HomePage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -18,9 +18,16 @@ export default function HomePage() {
           <TaskInput
             type="text"
             id="task"
+            list="task-suggestions"
             name="task"
             placeholder="Dê um nome para o seu projeto"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
@@ -28,6 +35,9 @@ export default function HomePage() {
             id="minutesAmount"
             name="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos.</span>
         </FormContainer>
