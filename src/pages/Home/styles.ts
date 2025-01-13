77a2d414869475746e0feb39582536120b7231f1
Thiago Leite <thiagofmleite@gmail.com>
2025-01-13 19:21:05 -0300

    feat: add styled input components for improved form functionality in HomePage

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -21,9 +21,38 @@ export const FormContainer = styled.div`
   justify-content: center;
   gap: 0.5rem;
   color: ${(props) => props.theme["gray-100"]};
-  font-size: 1.2rem;
+  font-size: 1.25rem;
   font-weight: bold;
   flex-wrap: wrap;
+`;
+
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme["gray-100"]};
+
+  &::placeholder {
+    color: ${(props) => props.theme["gray-500"]};
+    font-size: 1rem;
+  }
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme["green-500"]};
+  }
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
 `;
 
 export const CountdownContainer = styled.div`
