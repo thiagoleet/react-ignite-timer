06dc2daaaf59dc7edfc79fbcce4a76728418fd48
Thiago Leite <thiagofmleite@gmail.com>
2025-01-14 20:29:05 -0300

    feat: add ammountSecondsPast and updateAmmountSecondsPast to CyclesContext

--- src/pages/Home/context/CyclesContext.ts
+++ src/pages/Home/context/CyclesContext.ts
@@ -4,7 +4,9 @@ import { Cycle } from "../../../models/Cycle";
 export interface CyclesContextData {
   activeCycle: Cycle | undefined;
   activeCycleId: string | null;
+  ammountSecondsPast: number;
   markCurrentCycleAsFinished: VoidFunction;
+  updateAmmountSecondsPast: (ammount: number) => void;
 }
 
 export const CyclesContext = React.createContext<CyclesContextData>(
