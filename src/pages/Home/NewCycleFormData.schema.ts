d6fc56699d357219006e5117f94bc1dc0381245c
Thiago Leite <thiagofmleite@gmail.com>
2025-01-14 18:53:32 -0300

    feat: update minutesAmount validation to use constants for minimum and maximum values

--- src/pages/Home/NewCycleFormData.schema.ts
+++ src/pages/Home/NewCycleFormData.schema.ts
@@ -1,8 +1,14 @@
 import * as zod from "zod";
 
+export const MINIMUM_MINUTES_AMOUNT = 5;
+export const MAXIMUM_MINUTES_AMOUNT = 60;
+
 export const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
-  minutesAmount: zod.number().min(5).max(60),
+  minutesAmount: zod
+    .number()
+    .min(MINIMUM_MINUTES_AMOUNT)
+    .max(MAXIMUM_MINUTES_AMOUNT),
 });
 
 export type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
