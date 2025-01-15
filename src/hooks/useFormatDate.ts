2926e4274fcd24d54b46a3306e9923993def8830
Thiago Leite <thiagofmleite@gmail.com>
2025-01-15 09:08:33 -0300

    feat: update useFormatDate to handle null dates and improve date formatting

--- src/hooks/useFormatDate.ts
+++ src/hooks/useFormatDate.ts
@@ -4,21 +4,21 @@ import { ptBR } from "date-fns/locale/pt-BR";
 export function useFormatDate(date?: Date | null) {
   function formattedDate(inputDate: Date) {
     {
-      return format(inputDate || date, "dd 'de' MMMM 'às' HH:mm", {
+      return format(new Date(inputDate || date), "dd 'de' MMMM 'às' HH:mm", {
         locale: ptBR,
       });
     }
   }
 
-  function relativeDate(inputDate: Date) {
-    return formatDistanceToNow(inputDate || date, {
+  function relativeDate(inputDate?: Date) {
+    return formatDistanceToNow(new Date(inputDate ?? date!), {
       locale: ptBR,
       addSuffix: true,
     });
   }
 
-  function ISODate(inputDate: Date) {
-    return inputDate ? inputDate.toISOString() : date!.toISOString();
+  function ISODate(inputDate?: Date) {
+    return inputDate ? new Date(inputDate).toISOString() : date!.toISOString();
   }
 
   return { formattedDate, relativeDate, ISODate };
